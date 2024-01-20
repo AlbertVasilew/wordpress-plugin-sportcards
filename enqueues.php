e56d5895c13bc73bf25746f62eed7f42a0ff1f5d
unknown <bebosvg@abv.bg>
2024-01-20 10:23:31 +0200

    remove redundant scripts

--- enqueues.php
+++ enqueues.php
@@ -17,12 +17,10 @@
 
         wp_enqueue_style('sportcards-css', plugin_dir_url(__FILE__) . 'styles.css');
         wp_enqueue_style('cropper-css', 'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css');
-        wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css');
 
         wp_enqueue_script('player-card-js', plugin_dir_url(__FILE__) . 'scripts/player-card.js');
         wp_enqueue_script('cropper-manager-js', plugin_dir_url(__FILE__) . 'scripts/cropper-manager.js');
         wp_enqueue_script('cropper-js', 'https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.js', array('jquery'), '2.0.0', true);
-        wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js');
         wp_enqueue_script('sportcards-configurator-js', plugin_dir_url(__FILE__) . 'scripts/sportcards-configurator.js');
         wp_enqueue_script('price-calculator-js', plugin_dir_url(__FILE__) . 'scripts/price-calculator.js');
 
